846b5606ccdc7674a519e2b4a517e510fc4ccc36
zoostar <devops@zoostar.net>
2026-02-20 16:02:27 -0500

    LEZ-1: Add JaCoCo tests

--- src/main/java/com/zoostarinc/lez/web/controller/LezExceptionHandler.java
+++ src/main/java/com/zoostarinc/lez/web/controller/LezExceptionHandler.java
@@ -5,18 +5,15 @@ import java.security.GeneralSecurityException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import lombok.extern.slf4j.Slf4j;
 import net.zoostar.common.web.error.handler.CommonExceptionHandler;
 import net.zoostar.common.web.response.FailureResponseEntity;
 import net.zoostar.common.web.response.SystemFailureResponseEntity;
 
-@Slf4j
 @ControllerAdvice
 public class LezExceptionHandler extends CommonExceptionHandler {
 
 	@ExceptionHandler(GeneralSecurityException.class)
 	protected FailureResponseEntity handleException(Exception e) {
-		log.error(e.getMessage(), e);
 		return new SystemFailureResponseEntity(e.getMessage());
 	}
 
